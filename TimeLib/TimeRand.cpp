4eb32aebd52753cd667851a3bb03105745293df7
wy.exe <WY.exe@hotmail.com>
2018-10-20 15:26:04 +0800

    Change srand to __rdtsc

--- TimeLib/TimeRand.cpp
+++ TimeLib/TimeRand.cpp
@@ -1,11 +1,13 @@
 #include "TimeRand.h"
 #include "TimeTick.h"
+#include <random>
 
 int libTools::CTimeRand::GetRand(_In_ int nMinValue, _In_ int nMaxValue)
 {
-	static UINT uSeed = static_cast<UINT>(CTimeTick::_GetTickCount64());
-	srand(static_cast<UINT>(uSeed));
-	uSeed = static_cast<UINT>(rand());
+	//static UINT uSeed = static_cast<UINT>(CTimeTick::_GetTickCount64());
+	//srand(static_cast<UINT>(uSeed));
+	//uSeed = static_cast<UINT>(rand());
+	INT uSeed = static_cast<int>(__rdtsc());
 	return (nMinValue + uSeed % (nMaxValue - nMinValue + 1));
 }
 
